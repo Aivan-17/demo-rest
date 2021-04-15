be018686a86996818e3f0c2f96ec31dd0f8de2eb
Ivan Mamani <ivan.mamani.q@ucb.edu.bo>
2021-04-15 15:12:53 -0400

    1ra subida

---
+++ src/main/java/bo/edu/ucb/ingsoft/demorest/Recompensas.java
@@ -0,0 +1,13 @@
+package bo.edu.ucb.ingsoft.demorest;
+
+public class Recompensas {
+    Integer IdRecompensa;
+    Integer valorMin;
+    Integer valorMax;
+    String recompensa;
+    Integer Idproyecto;
+
+    public Recompensas(){
+
+    }
+}
